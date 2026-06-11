309fe42969a6e6d281371a1a2586582ebaffac13
sai konda <kondasaispoorthy@gmail.com>
2026-06-11 09:02:30 +0000

    added jaffle_shop source yaml file

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -2,16 +2,18 @@ with customers as (
 select * from {{ ref('stg_jaffle_shop__customers') }}
 ),
 orders as (
-    select * from {{ ref('stg_jaffle_shop__orders') }}    
+    select * from {{ ref('fct_orders') }}    
 ),
 customer_orders as (
     select customer_id,
     min(order_date) as first_order_date,
     max(order_date) as most_recent_order_date,
-    count(order_id) as number_of_orders
-    from orders
+    count(order_id) as number_of_orders,
+    sum(amount) as lifetime_value
+    from orders 
     group by 1
 ),
+
 final as (
     select *
     from customers 
