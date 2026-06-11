17a90f76b5b469fea6743b56aa6fb230e1435b56
sai konda <kondasaispoorthy@gmail.com>
2026-06-11 13:02:13 +0000

    added data tests

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,7 +3,8 @@ id as payment_id,
 orderid as order_id,
 paymentmethod as payment_method,
 status as payment_status,
-amount as payment_amount,
-created as payment_created,
+-- amount is stored in cents, convert it into dollars
+amount/100 as payment_amount,
+created as created_at,
 _batched_at
 from {{ source('stripe', 'payment') }}
